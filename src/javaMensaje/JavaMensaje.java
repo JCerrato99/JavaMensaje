22694d25d760285c9f5446ae9a04a69ccb120ba0
Administrador <Administrador@2DAM01>
2023-11-10 17:02:28 +0100

    Agrego Panel y agrando Titulo

--- src/javaMensaje/JavaMensaje.java
+++ src/javaMensaje/JavaMensaje.java
@@ -5,6 +5,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.JPanel;
+import java.awt.Component;
 
 public class JavaMensaje {
 
@@ -41,8 +44,14 @@ public class JavaMensaje {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLabel lblNewLabel = new JLabel("Titulo");
-		frame.getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+		JLabel titulo = new JLabel("Titulo");
+		titulo.setAlignmentX(Component.CENTER_ALIGNMENT);
+		titulo.setFont(new Font("Tahoma", Font.PLAIN, 49));
+		frame.getContentPane().add(titulo, BorderLayout.NORTH);
+		
+		JPanel panel_principal = new JPanel();
+		frame.getContentPane().add(panel_principal, BorderLayout.CENTER);
+		panel_principal.setLayout(new BorderLayout(0, 0));
 	}
 
 }
